7433a55c425cf571b480a8c4eee87058715bb29f
Guillermo Esteban <planeta.alderaan@gmail.com>
2024-10-18 15:23:21 +0200

    mejorar la librería para pasar nombre de usuario

--- src/saludo.php
+++ src/saludo.php
@@ -3,13 +3,15 @@ namespace Saludo\Saludo;
 
 Class Saludo{
     private $msn;
+    private $usuario;
 
-    public function __construct(){
+    public function __construct($usuario){
         $this->setSaludo();
+        $this->usuario = $usuario;
     }
 
     private function setSaludo(){
-        $this->msn = "Hola Mundo";
+        $this->msn = "Bienvenido, Sr./Sra.".$this->usuario;
     }
 
     public function getSaludo(){
